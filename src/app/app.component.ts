e7e612970042a5b2db4badbb2ba4fac2d51ec176
arjun3649 <arjun.rakwal3649@gmail.com>
2024-07-23 18:27:10 +0530

    added routes to the components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
+import { Component } from '@angular/core';
 import { RouterOutlet,RouterLink } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports:[RouterLink,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
