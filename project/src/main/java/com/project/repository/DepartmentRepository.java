031faeed173af8b50f586bc150714fbce6ea30d7
arjun3649 <arjun.rakwal3649@gmail.com>
2024-07-23 15:18:22 +0530

    Delete project directory

--- project/src/main/java/com/project/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.repository;
-
-import com.project.entity.DepartmentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<DepartmentEntity, Long> {
-}
--- project/src/main/java/com/project/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.entity.EmployeeEntity;
-
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
-}
